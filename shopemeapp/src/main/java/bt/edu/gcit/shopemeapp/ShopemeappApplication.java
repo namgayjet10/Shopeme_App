db70e6291c854aae1ca98b2caa771b36d21119ac
namgaywangchuk <12230029,gcit@rub.edu.bt>
2026-03-08 18:18:20 +0000

    first commit

---
+++ shopemeapp/src/main/java/bt/edu/gcit/shopemeapp/ShopemeappApplication.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.shopemeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopemeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopemeappApplication.class, args);
+	}
+
+}
---
+++ shopemeapp/src/test/java/bt/edu/gcit/shopemeapp/ShopemeappApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.shopemeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopemeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
